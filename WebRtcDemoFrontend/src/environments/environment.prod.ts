bc7501714fbaa09e8f4fde404fbcd294ecff8917
Evgeny Dedenko <evgenydedenko@gmail.com>
2021-08-14 04:50:04 +0300

    Add info for deploy.

--- WebRtcDemoFrontend/src/environments/environment.prod.ts
+++ WebRtcDemoFrontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleClientKey: '639115898353-o1un9ooq9lbf7bn6120kfktt65sdkpbg.apps.googleusercontent.com',
+  googleClientKey: '639115898353-oq2s5nj3qncab50im8c9ponrora3oi6v.apps.googleusercontent.com',
   googleScope: 'profile email',
-  apiDomain: "https://localhost:44318"
+  apiDomain: "https://api.gomel-dotnet.xyz"
 };
