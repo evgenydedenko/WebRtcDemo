35c7e2c70a30fbfd1398ef71b79447863015581c
Evgeny Dedenko <evgenydedenko@gmail.com>
2021-09-02 05:58:52 +0300

    fix columns

--- WebRtcDemoFrontend/src/environments/environment.prod.ts
+++ WebRtcDemoFrontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleClientKey: '639115898353-oq2s5nj3qncab50im8c9ponrora3oi6v.apps.googleusercontent.com',
+  googleClientKey: '639115898353-ggst0001r5hel59dhipmlvk052at6n8c.apps.googleusercontent.com',
   googleScope: 'profile email',
   apiDomain: "https://api.gomel-dotnet.xyz"
 };
